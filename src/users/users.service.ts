d984b413c30b2a2194cab3981a70a839f3ef2830
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-08-21 17:10:52 -0300

    feat(interceptors): adding not found interceptor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
 import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
@@ -15,8 +17,14 @@ export class UsersService {
     return this.repository.findAll();
   }
 
-  findOne(id: number) {
-    return this.repository.findOne(id);
+  async findOne(id: number): Promise<UserEntity> {
+    const user = await this.repository.findOne(id);
+
+    if (!user) {
+      throw new NotFoundError('Usuário não encontrado!');
+    }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
