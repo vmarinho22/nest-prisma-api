6c9125d0b283adae7ca719f5e0f902b56db19b64
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-08-21 15:01:51 -0300

    fix(users): fixing repository folder name

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
-import { UserEntity } from './../entities/user.entity';
+import { UserEntity } from '../entities/user.entity';
 
 @Injectable()
 export class UsersRepository {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersRepository } from './repositories/users.repository';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, UsersRepository]
 })
 export class UsersModule {}
