2b2fb849f74d2ae6f601c2b471f3e564956e1a07
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-08-22 22:08:01 -0300

    feat(docs): adding responses for swagger doc

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post
 } from '@nestjs/common';
+import { ApiForbiddenResponse } from '@nestjs/swagger';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostsService } from './posts.service';
@@ -15,26 +16,41 @@ import { PostsService } from './posts.service';
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Get()
   findAll() {
     return this.postsService.findAll();
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.postsService.findOne(+id);
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postsService.update(+id, updatePostDto);
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postsService.remove(+id);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post
 } from '@nestjs/common';
+import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
@@ -15,26 +16,45 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiResponse({
+    status: 409,
+    description: 'Usuário já existe na aplicação'
+  })
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @ApiForbiddenResponse({
+    description: 'Acesso negado'
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
