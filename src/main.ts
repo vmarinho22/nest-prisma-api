0f6d409503b015fe196f3792fd4ebfd2d361e3ae
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-08-22 21:52:23 -0300

    feat(docs): adding swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
@@ -20,6 +21,15 @@ async function bootstrap() {
   app.useGlobalInterceptors(new DatabaseInterceptor());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('The Simple Blog API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
